afbdc4f0a95af3d53bcbef230436a5d54e8692b4
enigma420 <enigma3420@gmail.com>
2019-08-21 14:12:49 +0200

    Create types and reducers for Event aspects

--- react_project/src/actions/types.js
+++ react_project/src/actions/types.js
@@ -2,3 +2,9 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_TEAMS = "GET_TEAMS";
 export const GET_TEAM = "GET_TEAM";
 export const DELETE_TEAM = "DELETE_TEAM";
+
+//Types for Eventslog actions
+
+export const GET_EVENTSLOG = "GET_EVENTSLOG";
+export const GET_EVENT = "GET_EVENT";
+export const DELETE_EVENT = "DELETE_EVENT";
--- react_project/src/reducers/index.js
+++ react_project/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import teamReducer from "./teamReducer";
+import eventslogReducer from "./eventslogReducer";
 
 export default combineReducers({
     errors: errorReducer,
-    team: teamReducer
+    team: teamReducer,
+    eventslog: eventslogReducer
 });
