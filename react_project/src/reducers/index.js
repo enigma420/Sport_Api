930e822eb96162cc6eab6f2948ae1aa10ed7edba
enigma420 <enigma3420@gmail.com>
2019-08-27 11:19:40 +0200

    Security Action and reducer with jwt-decode

--- react_project/src/reducers/index.js
+++ react_project/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import teamReducer from "./teamReducer";
 import eventslogReducer from "./eventslogReducer";
+import securityReducer from "./securityReducer";
 
 export default combineReducers({
     errors: errorReducer,
     team: teamReducer,
-    eventslog: eventslogReducer
+    eventslog: eventslogReducer,
+    security: securityReducer
 });
