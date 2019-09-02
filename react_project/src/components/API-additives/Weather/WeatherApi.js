eea2d227ec7c888cf8c36022a0ff8b49b2f2df1a
enigma420 <enigma3420@gmail.com>
2019-09-02 10:20:36 +0200

    Weather API update form

--- react_project/src/components/API-additives/Weather/WeatherApi.js
+++ react_project/src/components/API-additives/Weather/WeatherApi.js
@@ -7,20 +7,17 @@ const APIKey = '3bd17fcc0af94c30566296526e1f3429';
 
 
 class WeatherApi extends Component {
-
-    state = {
+constructor() {
+super();
+    this.state = {
         value: '',
         date: [],
         city: '',
-        temp: [
-            {
-                datetemp: '',
-                temp: ''
-            }
-
-        ],
+        temp: [],
+        weather: [],
         err: false,
     };
+}
 
     handleInputChange = (e) => {
         this.setState({
@@ -45,21 +42,10 @@ class WeatherApi extends Component {
             .then(data => {
                     this.setState(prevState => ({
                         err: false,
-
-                        temp: data.list.map(function (i, elem) {
-                            // if (i%2===0){
-                                return (
-                                    // {
-                                        datetemp: elem.dt_txt
-                                        // temp: elem.main.temp
-                                    // }
-
-                                )
-                            // }
-                        }),
+                        weather: data.list,
                         city: prevState.value,
                     }));
-                console.log("data.list:", data.list);
+                // console.log("data.list:", data.list);
             })
             .catch(err => {
                 console.log(err);
@@ -73,7 +59,38 @@ class WeatherApi extends Component {
 
     }
 
+
     render() {
+        const{weather} = this.state;
+        const weathermap = weather && weather.map(maps => (
+
+            <div>
+                <div className="card">
+                    <div className="card-header" id="headingOne">
+                        <h5 className="mb-0">
+                            <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                    aria-expanded="true" aria-controls="collapseOne">
+                                <h1>Date: {maps.dt_txt}</h1>
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
+                         data-parent="#accordion">
+                        <div className="card-body">
+                            <p>Temperature: {maps.main.temp} &#176;C</p>
+                            <p>Pressure: {maps.main.pressure} hPa</p>
+                            <p>Humidity: {maps.main.humidity} %</p>
+                            {/*<h2>Cloudy:{maps.weather.main} , {maps.weather.description}</h2>*/}
+                            <p>Percentage of cloud cover: {maps.clouds.all} %</p>
+                            <p>Wind speed: {maps.wind.speed} mph</p>
+                                                 </div>
+                    </div>
+
+
+            </div>
+            </div>
+                ));
         return (
             <div>
                 <WeatherForm
@@ -81,7 +98,10 @@ class WeatherApi extends Component {
                 change={this.handleInputChange}
                 //submit={this.handleCitySubmit}
                 />
-                <WeatherResult weather={this.state}/>
+                <div className="accordion">
+                {weathermap}
+                </div>
+                {/*<WeatherResult weather={this.state}/>*/}
             </div>
         );
     }
