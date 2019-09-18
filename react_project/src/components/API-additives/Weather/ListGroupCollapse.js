a28c8e49151897802cf20583c2dc2f3f48784438
Dominic <enigma3420@gmail.com>
2019-09-19 00:42:40 +0200

    second step of Weather API

--- react_project/src/components/API-additives/Weather/ListGroupCollapse.js
+++ react_project/src/components/API-additives/Weather/ListGroupCollapse.js
@@ -13,27 +13,29 @@ class ListGroupCollapse extends React.Component {
         this.setState({ collapse: !this.state.collapse });
     }
 
+    Approximately(n,k) {
+        var factor = Math.pow(10,k);
+        return Math.round(n*factor)/factor
+    }
+
     render() {
         const weatherParameters = this.props.parameters;
         return (
 
-            <ListGroupItem  style={{backgroundColor:'wheat' , borderRadius:'10px' , border:'3px solid black' , padding:'0px' , marginTop:'4px' , marginBottom:'4px' , pageBreakInside: 'avoid'}}>
-                <div>
+            <ListGroupItem style={{backgroundColor:'wheat' , borderRadius:'10px' , border:'3px solid black' , margin:'5px' ,  pageBreakInside: 'avoid' , minWidth:'155px' , maxHeight:'280px' }}>
                     <p onClick={this.toggle}>
-                        <h3><b>{weatherParameters.dt_txt}</b></h3>
+                        <h4><b>{weatherParameters.dt_txt}</b></h4>
 
                     </p>
 
                     <Collapse isOpen={this.state.collapse}>
                         <hr/>
-                    <p>Temperature: {weatherParameters.main.temp} &#176;C</p>
-                    <p>Pressure: {weatherParameters.main.pressure} hPa</p>
-                    <p>Humidity: {weatherParameters.main.humidity} %</p>
-                    <p>Cloud cover: {weatherParameters.clouds.all} %</p>
-                    <p>Wind speed: {weatherParameters.wind.speed} mph</p>
-                        <hr/>
+                    <p>Temperature: {this.Approximately(weatherParameters.main.temp,0)}&#176;C</p>
+                    <p>Pressure: {this.Approximately(weatherParameters.main.pressure,0)}hPa</p>
+                    <p>Humidity: {weatherParameters.main.humidity}%</p>
+                    <p>Cloud: {weatherParameters.clouds.all}%</p>
+                    <p>Wind: {weatherParameters.wind.speed} mph</p>
                     </Collapse>
-                </div>
             </ListGroupItem>
 
         );
--- react_project/src/components/API-additives/Weather/WeatherApi.js
+++ react_project/src/components/API-additives/Weather/WeatherApi.js
@@ -3,6 +3,8 @@ import WeatherForm from "./WeatherForm";
 import ListGroupCollapse from "./ListGroupCollapse";
 import Container from "reactstrap/es/Container";
 import {Collapse} from "reactstrap";
+import NumberFormat from 'react-number-format';
+import moment from 'moment'
 
 //API access key:
 const APIKey = '3bd17fcc0af94c30566296526e1f3429';
@@ -67,13 +69,167 @@ super();
 
         return (
 
-            <div style={{ marginRight:'10px' , marginLeft:'10px' , backgroundColor:'lightskyblue' , borderRadius:'20px'}}>
+            <div style={{  backgroundColor:'lightskyblue' , borderRadius:'25px'}}>
 
                 {Object.keys(weatherParameters).map((key, index) =>
                     <ListGroupCollapse key={index} parameters={weatherParameters[key]}/>
                 )}
             </div>
             )
+    };
+
+    changeTimeZoneIntoGMT = (timeZone) => {
+        if(timeZone === -43200){
+            return <span>GMT-12</span>
+        }
+        if(timeZone === -41400){
+            return <span>GMT-11.5</span>
+        }
+        if(timeZone === -39600){
+            return <span>GMT-11</span>
+        }
+        if(timeZone === -37800){
+            return <span>GMT-10.5</span>
+        }
+        if(timeZone === -36000){
+            return <span>GMT-10</span>
+        }
+        if(timeZone === -34200){
+            return <span>GMT-9.5</span>
+        }
+        if(timeZone === -32400){
+            return <span>GMT-9</span>
+        }
+        if(timeZone === -30600){
+            return <span>GMT-8.5</span>
+        }
+        if(timeZone === -28800){
+            return <span>GMT-8</span>
+        }
+        if(timeZone === -27000){
+            return <span>GMT-7.5</span>
+        }
+        if(timeZone === -25200){
+            return <span>GMT-7</span>
+        }
+        if(timeZone === -23400){
+            return <span>GMT-6.5</span>
+        }
+        if(timeZone === -21600){
+            return <span>GMT-6</span>
+        }
+        if(timeZone === -19800){
+            return <span>GMT-5.5</span>
+        }
+        if(timeZone === -18000){
+            return <span>GMT-5</span>
+        }
+        if(timeZone === -16200){
+            return <span>GMT-4.5</span>
+        }
+        if(timeZone === -14400){
+            return <span>GMT-4</span>
+        }
+        if(timeZone === -12600){
+            return <span>GMT-3.5</span>
+        }
+        if(timeZone === -10800){
+            return <span>GMT-3</span>
+        }
+        if(timeZone === -9000){
+            return <span>GMT-2.5</span>
+        }
+        if(timeZone === -7200){
+            return <span>GMT-2</span>
+        }
+        if(timeZone === -5400){
+            return <span>GMT-1.5</span>
+        }
+        if(timeZone === -3600){
+            return <span>GMT-1</span>
+        }
+        if(timeZone === -1800){
+            return <span>GMT-0.5</span>
+        }
+        if(timeZone === 0){
+            return <span>GMT 0</span>
+        }
+        if(timeZone === 1800){
+            return <span>GMT+0.5</span>
+        }
+        if(timeZone === 3600){
+            return <span>GMT+1</span>
+        }
+        if(timeZone === 5400){
+            return <span>GMT+1.5</span>
+        }
+        if(timeZone === 7200){
+            return <span>GMT+2</span>
+        }
+        if(timeZone === 9000){
+            return <span>GMT+2.5</span>
+        }
+        if(timeZone === 10800){
+            return <span>GMT+3</span>
+        }
+        if(timeZone === 12600){
+            return <span>GMT+3.5</span>
+        }
+        if(timeZone === 14400){
+            return <span>GMT+4</span>
+        }
+        if(timeZone === 16200){
+            return <span>GMT+4.5</span>
+        }
+        if(timeZone === 18000){
+            return <span>GMT+5</span>
+        }
+        if(timeZone === 19800){
+            return <span>GMT+5.5</span>
+        }
+        if(timeZone === 21600){
+            return <span>GMT+6</span>
+        }
+        if(timeZone === 23400){
+            return <span>GMT+6.5</span>
+        }
+        if(timeZone === 25200){
+            return <span>GMT+7</span>
+        }
+        if(timeZone === 27000){
+            return <span>GMT+7.5</span>
+        }
+        if(timeZone === 28800){
+            return <span>GMT+8</span>
+        }
+        if(timeZone === 30600){
+            return <span>GMT+8.5</span>
+        }
+        if(timeZone === 32400){
+            return <span>GMT+9</span>
+        }
+        if(timeZone === 34200){
+            return <span>GMT+9.5</span>
+        }
+        if(timeZone === 36000){
+            return <span>GMT+10</span>
+        }
+        if(timeZone === 37800){
+            return <span>GMT+10.5</span>
+        }
+        if(timeZone === 39600){
+            return <span>GMT+11</span>
+        }
+        if(timeZone === 41400){
+            return <span>GMT+11.5</span>
+        }
+        if(timeZone === 43200){
+            return <span>GMT+12</span>
+        }
+    };
+
+    changeTimeStampIntoDateTime = (timeStamp) => {
+        return <span>{timeStamp.moment().format('lll')}</span>
     };
 
     getCityParameters = () => {
@@ -85,15 +241,17 @@ super();
         if (!error && city) {
 
             content = (
-                <div className="list-group-country" style={{ marginTop:'10px' , marginRight:'180px' , marginLeft:'180px' , backgroundColor:'lightskyblue' , borderRadius:'20px' , border:'5px solid seagreen' , padding:'1px'}}>
+                <div className="list-group-country" style={{ marginTop:'10px' , marginRight:'120px' , marginLeft:'120px' , backgroundColor:'lightskyblue' , borderRadius:'20px' , border:'5px solid seagreen' , padding:'1px'}}>
                     <div className="cityApiResult" >
                         <h2><b>Detected City: {cityParameters.name}</b></h2>
                         <hr/>
                         <h3><b>Country Code: {cityParameters.country}</b></h3>
-                        <h4><b>Population: {cityParameters.population}</b></h4>
-                        <h4><b>TimeZone: {cityParameters.timezone}</b></h4>
-                        <h4><b>Sunrise: {cityParameters.sunrise}</b></h4>
-                        <h4><b>Sunset: {cityParameters.sunset}</b></h4>
+                        <h4><b>Population: <NumberFormat value={cityParameters.population} displayType={'text'} thousandSeparator={true}/>
+
+                        </b></h4>
+                        <h4><b>Time-Zone: {this.changeTimeZoneIntoGMT(cityParameters.timezone)}</b></h4>
+                        <h4><b>Sunrise: {this.changeTimeStampIntoDateTime(cityParameters.sunrise)}</b></h4>
+                        <h4><b>Sunset: {this.changeTimeStampIntoDateTime(cityParameters.sunset)}</b></h4>
                         <hr/>
                     </div>
                 </div>
@@ -109,7 +267,7 @@ super();
 
     render() {
         return (
-            <div className="container" style={{position:"fixed"}}>
+            <div style={{ minHeight:'80%' , marginBottom:'8%'}}>
             <WeatherForm
     value={this.state.value}
     change={this.handleInputChange}
